29fddc1d5ecd2684925eb333540c73d2ef786dcb
Zomer <zomerzxc.19@gmail.com>
2021-07-29 21:09:51 +0800

    Fix overall layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,9 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import { Hero } from '../src/components/index';
+import { Layout } from '../src/components/index';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Zomer Gregorio</title>
-        <meta name='description' content='Zomer Gregorio Portfolio' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main className={styles.main}>
-        <Hero />
-      </main>
-    </div>
+    <>
+      <Layout />
+    </>
   );
 }
