9a74b90988557182e0046e16eacd8554d6bbf78e
Zomer <zomerzxc.19@gmail.com>
2021-07-29 21:09:26 +0800

    Add nav links to configs

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+import { INavLinks } from './types';
+
+export const navLinks: INavLinks[] = [
+  {
+    name: 'About',
+    url: '#about',
+  },
+  {
+    name: 'Experience',
+    url: '#experience',
+  },
+  {
+    name: 'Projects',
+    url: '#projects',
+  },
+  {
+    name: 'Contact',
+    url: '#contact',
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface INavLinks {
+  name: string;
+  url: string;
+}
