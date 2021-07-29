617c4b14d83f0f323109863f6b57086a546a2d3f
Zomer <zomerzxc.19@gmail.com>
2021-07-29 17:21:40 +0800

    Initial commit - Create Next App

---
+++ src/components/index.tsx
@@ -0,0 +1,1 @@
+export { default as Hero } from './sections/Hero';
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface IHeroProps {}
+
+const Hero: React.FC<IHeroProps> = ({}) => {
+  return <div>ZOMER</div>;
+};
+
+export default Hero;
