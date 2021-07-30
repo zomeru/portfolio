dd22dd0b961711118a309be60029a49a83446c6f
Zomer <zomerzxc.19@gmail.com>
2021-07-31 01:15:29 +0800

    Move global styles and async load font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -34,9 +35,24 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
+            rel='preload'
+            as='style'
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap'
-            rel='stylesheet'
           />
+
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css2?family=Poppins&display=swap'
+            media='print'
+            // eslint-disable-next-line react/no-unknown-property
+            onload="this.media='all'"
+          />
+          <noscript>
+            <link
+              href='https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap'
+              rel='stylesheet'
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import { Layout, About, Hero } from '../src/components/index';
+import { GlobalStyles } from '../src/styles';
 
 export default function Home() {
   return (
-    <Layout>
-      <Hero />
-      <About />
-    </Layout>
+    <>
+      <GlobalStyles />
+      <Layout>
+        <Hero />
+        <About />
+      </Layout>
+    </>
   );
 }
