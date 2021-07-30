c7109d26880548c90d2abacf52454a81dedd7f8a
Zomer <zomerzxc.19@gmail.com>
2021-07-30 20:00:59 +0800

    Remove font import

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,3 @@
 export { default as GlobalStyles } from './GlobalStyles';
-export { default as Fonts } from './fonts';
+// export { default as Fonts } from './fonts';
 export { default as Variables } from './variables';
