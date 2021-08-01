f40e86638f1b3248cb1f965b8ddb5a5f092a39db
Zomer <zomerzxc.19@gmail.com>
2021-08-01 20:06:08 +0800

    Add skills config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import { INavLinks } from './types';
-import { IoLogoLinkedin } from 'react-icons/io';
 
 export const navLinks: INavLinks[] = [
   {
@@ -32,5 +31,48 @@ export const floatingLinks: INavLinks[] = [
   {
     name: 'instagram',
     url: 'https://instagram.com/zomerusama',
+  },
+];
+
+export const skills = [
+  {
+    name: 'HTML5',
+    color: '#e34c26',
+  },
+  {
+    name: 'Sass',
+    color: '#c69',
+  },
+  {
+    name: 'Javascript',
+    color: '#F0DB4F',
+  },
+  {
+    name: 'Typescript',
+    color: '#007acc',
+  },
+  {
+    name: 'React.js',
+    color: '#61DBFB',
+  },
+  {
+    name: 'Tailwind CSS',
+    color: 'rgb(6, 182, 212)',
+  },
+  {
+    name: 'Node.js',
+    color: '#539e43',
+  },
+  {
+    name: 'Firebase',
+    color: '#FFCB2B',
+  },
+  {
+    name: 'Github',
+    color: '#211f1f',
+  },
+  {
+    name: 'Git',
+    color: '#F1502F',
   },
 ];
