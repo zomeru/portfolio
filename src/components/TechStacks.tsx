d195d3f6b80d022cff1b51543f4b11ff0d6f8fb4
Zomer <zomerzxc.19@gmail.com>
2021-08-01 20:05:39 +0800

    Add Techstacks

--- src/components/TechStacks.tsx
+++ src/components/TechStacks.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+// @ts-ignore
+import { AiFillGithub, AiFillHtml5 } from 'react-icons/ai';
+import { IoLogoSass } from 'react-icons/io';
+import {
+  SiTypescript,
+  SiTailwindcss,
+  SiFirebase,
+  SiJavascript,
+} from 'react-icons/si';
+import { FaReact, FaNodeJs, FaGitAlt } from 'react-icons/fa';
+import { skills } from '../config';
 
 const StyledTechStacks = styled.div`
   height: 100vh;
@@ -12,14 +23,97 @@ const StyledTechStacks = styled.div`
   }
 `;
 
-interface ITechStacksProps {}
+const StyledTech = styled.div`
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  margin-top: var(--mg-lg);
 
-const TechStacks: React.FC<ITechStacksProps> = ({}) => {
+  ul {
+    max-width: 60%;
+    display: flex;
+    /* justify-content: space-evenly; */
+    flex-flow: wrap;
+    justify-content: center;
+  }
+
+  li {
+    border: 1px solid var(--gray-dark);
+    height: 55px;
+    width: auto;
+    border-radius: 5px;
+    overflow: hidden;
+    display: inline-block;
+    margin-bottom: 15px;
+
+    :not(:last-child) {
+      margin-right: 30px;
+    }
+
+    .tech-content {
+      display: flex;
+      align-items: center;
+      height: 100%;
+
+      .logo {
+        margin-left: 10px;
+        margin-right: 20px;
+        width: 30px;
+        height: 30px;
+      }
+
+      p {
+        margin-right: 10px;
+      }
+    }
+  }
+`;
+
+const TechStacks = () => {
+  const html5 = <AiFillHtml5 className='logo' />;
+  const sass = <IoLogoSass className='logo' />;
+  const js = <SiJavascript className='logo' />;
+  const ts = <SiTypescript className='logo' />;
+  const react = <FaReact className='logo' />;
+  const tailwind = <SiTailwindcss className='logo' />;
+  const node = <FaNodeJs className='logo' />;
+  const firebase = <SiFirebase className='logo' />;
+  const github = <AiFillGithub className='logo' />;
+  const git = <FaGitAlt className='logo' />;
+
+  const iconList = [
+    html5,
+    sass,
+    js,
+    ts,
+    react,
+    tailwind,
+    node,
+    firebase,
+    github,
+    git,
+  ];
+
   return (
     <StyledTechStacks>
-      <h1>TECH STACKS</h1>
-      <p>Here are some technologies I&rsquo;ve been working with recently.</p>
-      <div></div>
+      <h1>SKILLS</h1>
+      <p>
+        Here are some technologies I use for building awesome web applications.
+      </p>
+      <StyledTech>
+        <ul>
+          {skills.map((skill, index) => {
+            return (
+              <li key={skill.name}>
+                <div className='tech-content'>
+                  <div style={{ color: skill.color }}>{iconList[index]}</div>
+                  <p>{skill.name}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </StyledTech>
     </StyledTechStacks>
   );
 };
