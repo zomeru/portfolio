f22f5b07aa0451eb0c8c9e76f00f80f39760cc48
Zomer <zomerzxc.19@gmail.com>
2021-08-02 04:48:43 +0800

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -52,6 +52,25 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap'
             rel='stylesheet'
           />
+          <link rel='shortcut icon' href='/favicon.ico' />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
         </Head>
         <body>
           <Main />
