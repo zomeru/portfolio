d75af756facf42ea204ddec09f036569784447ad
Zomer <zomerzxc.19@gmail.com>
2021-08-03 20:18:23 +0800

    Add media queries and some tweaks

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
-import { IoLogoLinkedin } from 'react-icons/io';
 import { Button } from '../index';
 import { floatingLinks } from '../../data';
 import styled from 'styled-components';
@@ -19,30 +17,55 @@ const StyledHeroContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  @media only screen and (max-width: 1060px) {
+    padding: 0 80px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 0 50px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    padding: 0 30px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    padding: 0;
+  }
 `;
 
 const StyledHeroContent = styled.div`
   position: relative;
-  height: calc(70%);
+  height: 70%;
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  @media only screen and (max-width: 1000px) {
+    text-align: center;
+  }
 
   h1 {
     font-weight: var(--font-regular);
     font-size: var(--fz-xxl);
     margin-bottom: var(--mg-md);
+
+    @media only screen and (max-width: 600px) {
+      font-size: var(--fz-xl);
+    }
   }
 
   h2 {
     margin-left: -8px;
     font-weight: var(--font-bold);
-    font-size: 85px;
+    /* font-size: 85px; */
+    font-size: clamp(24px, 7vw, 85px);
     margin-bottom: var(--mg-sm);
     position: relative;
     max-width: 720px;
 
-    :hover {
+    /* :hover {
       color: var(--gray-light);
     }
 
@@ -63,15 +86,20 @@ const StyledHeroContent = styled.div`
 
     :hover::before {
       width: 100%;
-    }
+    } */
   }
 
   h3 {
     margin-left: var(--mg-xl);
     font-weight: var(--font-bold);
-    font-size: 55px;
+    /* font-size: 55px; */
+    font-size: clamp(20px, 5vw, 55px);
     margin-bottom: var(--mg-md);
     color: var(--gray-light);
+
+    @media only screen and (max-width: 1000px) {
+      margin-left: 0;
+    }
   }
 
   .four {
@@ -79,11 +107,20 @@ const StyledHeroContent = styled.div`
     font-weight: var(--font-medium);
     margin-left: var(--mg-xxl);
     color: var(--gray-light);
+
+    @media only screen and (max-width: 1000px) {
+      margin-left: 0;
+      max-width: 100%;
+    }
   }
 
   .hero-button {
     margin-left: var(--mg-xxl);
     margin-top: var(--mg-lg);
+
+    @media only screen and (max-width: 1000px) {
+      margin: var(--mg-lg) auto 0 auto;
+    }
   }
 
   .icon-container {
@@ -108,6 +145,10 @@ const StyledHeroContent = styled.div`
     width: 80px;
     height: 80px;
     transition: all 0.2s ease-in-out;
+
+    @media only screen and (max-width: 1000px) {
+      display: none;
+    }
 
     :hover {
       background-color: rgba(144, 202, 249, 0.35);
