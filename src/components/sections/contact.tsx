b7cabf8221c3eea53b8c321016177595531b111b
Zomer <zomerzxc.19@gmail.com>
2021-08-03 22:07:00 +0800

    Add media queries

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -8,9 +8,48 @@ const StyledContact = styled.section`
   text-align: center;
   /* margin: 0 auto; */
   margin-bottom: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media only screen and (max-width: 1400px) {
+    padding: 50px 20%;
+  }
+
+  @media only screen and (max-width: 1080px) {
+    padding: 50px 10%;
+  }
+
+  @media only screen and (max-width: 1000px) {
+    padding: 50px 30px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 50px 0;
+  }
+
+  h2 {
+    @media only screen and (max-width: 768px) {
+      line-height: 0.8;
+    }
+
+    @media only screen and (max-width: 600px) {
+      line-height: 0.6;
+    }
+
+    @media only screen and (max-width: 600px) {
+      font-size: 22px;
+      line-height: 0.4;
+    }
+  }
 
   .contact-p {
     color: var(--gray-light);
+    width: 85%;
+
+    @media only screen and (max-width: 600px) {
+      width: 100%;
+    }
   }
 
   .message-button {
@@ -24,9 +63,9 @@ interface IcontactProps {}
 const Contact: React.FC<IcontactProps> = ({}) => {
   return (
     <StyledContact id='contact'>
-      <h1 className='section-heading'>
+      <h2 className='section-heading'>
         I occasionally take on freelance opportunities
-      </h1>
+      </h2>
       <p className='contact-p'>
         My inbox is always open. Whether you have a project where you need some
         help or just want to tell me something, send me over a message and
