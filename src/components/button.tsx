a81844d2833623286a135a8c562b82410ff1bb34
Zomer <zomerzxc.19@gmail.com>
2021-08-04 18:08:22 +0800

    Add props to logically render Link or a button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyledButton } from '../styles/componentStyles';
+import Link from 'next/link';
 
 interface IButton {
   buttonText: string;
@@ -7,6 +8,7 @@ interface IButton {
   className?: string;
   target?: string;
   rel?: string;
+  isLink: boolean;
 }
 
 const Button: React.FC<IButton> = ({
@@ -15,17 +17,29 @@ const Button: React.FC<IButton> = ({
   className,
   target,
   rel,
+  isLink,
 }) => {
   return (
-    <StyledButton
-      href={buttonUrl}
-      target={target}
-      rel={rel}
-      className={className}
-    >
-      <div className='line'></div>
-      <p>{buttonText}</p>
-    </StyledButton>
+    <>
+      {isLink ? (
+        <Link href={buttonUrl} passHref>
+          <StyledButton className={className}>
+            <div className='line'></div>
+            <p>{buttonText}</p>
+          </StyledButton>
+        </Link>
+      ) : (
+        <StyledButton
+          href={buttonUrl}
+          target={target}
+          rel={rel}
+          className={className}
+        >
+          <div className='line'></div>
+          <p>{buttonText}</p>
+        </StyledButton>
+      )}
+    </>
   );
 };
 
