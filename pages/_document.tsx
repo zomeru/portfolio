7cf88c536e2283a2f95cf87adbd8acaa32921ac2
Zomer <zomerzxc.19@gmail.com>
2021-08-07 03:05:23 +0800

    Little tweaks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,77 +50,80 @@ class MyDocument extends Document {
           <link
             rel='apple-touch-icon'
             sizes='57x57'
-            href='icons/apple-icon-57x57.png'
+            href='/icons/apple-icon-57x57.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='60x60'
-            href='icons/apple-icon-60x60.png'
+            href='/icons/apple-icon-60x60.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='72x72'
-            href='icons/apple-icon-72x72.png'
+            href='/icons/apple-icon-72x72.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='76x76'
-            href='icons/apple-icon-76x76.png'
+            href='/icons/apple-icon-76x76.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='114x114'
-            href='icons/apple-icon-114x114.png'
+            href='/icons/apple-icon-114x114.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='120x120'
-            href='icons/apple-icon-120x120.png'
+            href='/icons/apple-icon-120x120.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='144x144'
-            href='icons/apple-icon-144x144.png'
+            href='/icons/apple-icon-144x144.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='152x152'
-            href='icons/apple-icon-152x152.png'
+            href='/icons/apple-icon-152x152.png'
           />
           <link
             rel='apple-touch-icon'
             sizes='180x180'
-            href='icons/apple-icon-180x180.png'
+            href='/icons/apple-icon-180x180.png'
           />
           <link
             rel='icon'
             type='image/png'
             sizes='192x192'
-            href='icons/android-icon-192x192.png'
+            href='/icons/android-icon-192x192.png'
           />
           <link
             rel='icon'
             type='image/png'
             sizes='32x32'
-            href='icons/favicon-32x32.png'
+            href='/icons/favicon-32x32.png'
           />
           <link
             rel='icon'
             type='image/png'
             sizes='96x96'
-            href='icons/favicon-96x96.png'
+            href='/icons/favicon-96x96.png'
           />
           <link
             rel='icon'
             type='image/png'
             sizes='16x16'
-            href='icons/favicon-16x16.png'
+            href='/icons/favicon-16x16.png'
           />
           <link rel='manifest' href='/site.webmanifest' />
           <meta name='msapplication-TileColor' content='#102039' />
-          <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
+          <meta
+            name='msapplication-TileImage'
+            content='/icons/ms-icon-144x144.png'
+          />
           <meta name='theme-color' content='#102039' />
-          <link rel='mask-icon' href='icons/mask-icon.svg' color='#102039' />
+          <link rel='mask-icon' href='/icons/mask-icon.svg' color='#102039' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
             rel='preconnect'
