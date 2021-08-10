9084a8bf07695f2d76f2cee9c405abde2ba5dc4e
Zomer <zomerzxc.19@gmail.com>
2021-08-11 02:28:54 +0800

    Optimize images on project section

---
+++ src/assets/images/projects/index.ts
@@ -0,0 +1,4 @@
+export { default as zomify } from './zomify-colors-min.webp';
+export { default as portfolio } from './simple-portfolio-min.webp';
+export { default as forkify } from './forkify-min.webp';
+export { default as pigdice } from './pig-dice-game-min.webp';
