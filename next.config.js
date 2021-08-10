6e491f2513aee96b812a916db2029a256f57cfef
Zomer <zomerzxc.19@gmail.com>
2021-08-11 07:37:18 +0800

    Add redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,38 @@ module.exports = {
   reactStrictMode: true,
 };
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/facebook',
+        destination: 'https://facebook.com/Zomeru',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/zomerusama',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/zomeru_sama',
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://linkedin.com/in/zomergregorio',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/zomeru',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 const path = require('path');
 const withPWA = require('next-pwa');
 // const runtimeCaching = require('next-pwa/cache');
