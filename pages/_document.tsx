ef1098028317a9bf5ec18e412d1e96b50660c154
Zomer <zomerzxc.19@gmail.com>
2021-08-14 07:41:02 +0800

    Little tweaks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -135,19 +135,21 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap'
             rel='preload'
             as='style'
+            crossOrigin='anonymous'
           />
           <link
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap'
             rel='stylesheet'
             media='print'
             onLoad="this.media='all'"
+            crossOrigin='anonymous'
           />
-          <noscript>
+          {/* <noscript>
             <link
               rel='stylesheet'
               href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap'
             />
-          </noscript>
+          </noscript> */}
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
