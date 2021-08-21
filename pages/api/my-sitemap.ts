a5fb5cee377b57ba6b2ccc8bb7559da73ac87f2b
Zomer <zomerzxc.19@gmail.com>
2021-08-21 12:14:57 +0800

    Add dynamic sitemap

---
+++ pages/api/my-sitemap.ts
@@ -0,0 +1,21 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { Readable } = require('stream');
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: any, res: any) => {
+  // An array with your links
+  const links = [{ url: '/', changefreq: 'daily', priority: 0.3 }];
+
+  // Create a stream to write to
+  const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
+
+  res.writeHead(200, {
+    'Content-Type': 'application/xml',
+  });
+
+  const xmlString = await streamToPromise(
+    Readable.from(links).pipe(stream)
+  ).then((data: any) => data.toString());
+
+  res.end(xmlString);
+};
