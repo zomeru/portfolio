9f682e0430b1753a5661a0b9dc2200a52928b09f
Zomer <zomerzxc.19@gmail.com>
2021-09-10 21:26:01 +0800

    Little tweaks

--- next.config.js
+++ next.config.js
@@ -18,37 +18,31 @@ const nextRedirect = {
         source: '/facebook',
         destination: 'https://facebook.com/Zomeru',
         permanent: false,
-        basePath: false,
       },
       {
         source: '/instagram',
         destination: 'https://instagram.com/zomerusama',
         permanent: false,
-        basePath: false,
       },
       {
         source: '/tiktok',
         destination: 'https://tiktok.com/@zomeru_sama',
         permanent: false,
-        basePath: false,
       },
       {
         source: '/twitter',
         destination: 'https://twitter.com/zomeru_sama',
         permanent: false,
-        basePath: false,
       },
       {
         source: '/linkedin',
         destination: 'https://linkedin.com/in/zomergregorio',
         permanent: false,
-        basePath: false,
       },
       {
         source: '/github',
         destination: 'https://github.com/zomeru',
         permanent: false,
-        basePath: false,
       },
     ];
   },
