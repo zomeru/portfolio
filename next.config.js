88da7e6bb84762f0e7af61bbd8b1b16f4b93feac
Zomer <zomerzxc.19@gmail.com>
2021-09-10 15:40:20 +0800

    Fix next-config

--- next.config.js
+++ next.config.js
@@ -2,63 +2,70 @@ module.exports = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/facebook',
-        destination: 'https://facebook.com/Zomeru',
-        permanent: false,
-        basePath: false,
-      },
-      {
-        source: '/instagram',
-        destination: 'https://instagram.com/zomerusama',
-        permanent: false,
-        basePath: false,
-      },
-      {
-        source: '/tiktok',
-        destination: 'https://tiktok/@zomeru_sama',
-        permanent: false,
-        basePath: false,
-      },
-      {
-        source: '/twitter',
-        destination: 'https://twitter.com/zomeru_sama',
-        permanent: false,
-        basePath: false,
-      },
-      {
-        source: '/linkedin',
-        destination: 'https://linkedin.com/in/zomergregorio',
-        permanent: false,
-        basePath: false,
-      },
-      {
-        source: '/github',
-        destination: 'https://github.com/zomeru',
-        permanent: false,
-        basePath: false,
-      },
-    ];
-  },
-};
-
+const withPlugins = require('next-compose-plugins');
 const path = require('path');
 const withPWA = require('next-pwa');
 // const runtimeCaching = require('next-pwa/cache');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    // swSrc: 'sw.js',
-    register: true,
-    skipWaiting: true,
-    // runtimeCaching,
-  },
-  webpack: config => {
-    config.resolve.modules.push(path.resolve('./'));
-    return config;
-  },
-});
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public',
+          // swSrc: 'sw.js',
+          register: true,
+          skipWaiting: true,
+          // runtimeCaching,
+        },
+        webpack: config => {
+          config.resolve.modules.push(path.resolve('./'));
+          return config;
+        },
+      },
+    ],
+  ],
+  {
+    async redirects() {
+      return [
+        {
+          source: '/facebook',
+          destination: 'https://facebook.com/Zomeru',
+          permanent: false,
+          basePath: false,
+        },
+        {
+          source: '/instagram',
+          destination: 'https://instagram.com/zomerusama',
+          permanent: false,
+          basePath: false,
+        },
+        {
+          source: '/tiktok',
+          destination: 'https://tiktok/@zomeru_sama',
+          permanent: false,
+          basePath: false,
+        },
+        {
+          source: '/twitter',
+          destination: 'https://twitter.com/zomeru_sama',
+          permanent: false,
+          basePath: false,
+        },
+        {
+          source: '/linkedin',
+          destination: 'https://linkedin.com/in/zomergregorio',
+          permanent: false,
+          basePath: false,
+        },
+        {
+          source: '/github',
+          destination: 'https://github.com/zomeru',
+          permanent: false,
+          basePath: false,
+        },
+      ];
+    },
+  }
+);
