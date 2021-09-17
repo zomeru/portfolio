d37a6254f59cec3813645c93514236fc82eefe3d
Zomer <zomerzxc.19@gmail.com>
2021-09-18 04:14:42 +0800

    Fix loader issue in blog route

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,10 +4,13 @@ import BlogLayout from '@components/blogLayout';
 interface indexProps {
   theme: string;
   toggleTheme: () => void;
-  isHome: boolean;
+  // isHome: boolean;
 }
 
-const index: React.FC<indexProps> = ({ theme, toggleTheme, isHome }) => {
+const index: React.FC<indexProps> = ({ theme, toggleTheme }) => {
+  // const router = useRouter();
+  const isHome = window.location.pathname === '/';
+
   return (
     <BlogLayout isHome={isHome} theme={theme} toggleTheme={toggleTheme}>
       <h2 className='section-heading'>My Blog Posts</h2>
